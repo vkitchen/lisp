7b606e9199b90cfb77a0087f172bff19709891b9
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-01-21 10:33:37 +1300

    dynamic array stub

---
+++ source/dynarray.c
@@ -0,0 +1,6 @@
+/*
+		DYNARRAY.C
+		------
+		Copyright (c) 2017 Vaughan Kitchen
+		Released under the MIT license (https://opensource.org/licenses/MIT)
+*/
---
+++ source/dynarray.h
@@ -0,0 +1,13 @@
+/*
+		DYNARRAY.H
+		------
+		Copyright (c) 2017 Vaughan Kitchen
+		Released under the MIT license (https://opensource.org/licenses/MIT)
+*/
+
+#ifndef DYNARRAY_H_
+#define DYNARRAY_H_
+
+typedef struct dynarray_noderec *dynarray_node;
+
+#endif
